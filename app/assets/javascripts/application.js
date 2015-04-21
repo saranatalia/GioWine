f2a88b425ce46b81efe747f9421defec79f2717f
Sara Cardullo <saranataliacardullo@gmail.com>
2015-04-21 22:00:08 +0200

    Changing navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,20 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-// require_self
+//= require_self
+
+  var start_to_fix_header = 30;
+  var calc_for_fixed_report = function() {
+    var current_scroll_top = $(window).scrollTop();
+    var current_position = $('#header').css('position');
+    if ((current_scroll_top > start_to_fix_header) && (current_position != 'fixed')) {
+      $('#header').addClass('fixed');
+      $('#saracontent').css('margin-top', $('#header').height()+($('#header').height()/2));
+    }
+    if ((current_scroll_top < (start_to_fix_header + 1)) && (current_position == 'fixed')) {
+      $('#header').removeClass('fixed');
+      $('#saracontent').css('margin-top', 0);
+    }
+  };
+  $(window).scroll(calc_for_fixed_report);
+  calc_for_fixed_report();
